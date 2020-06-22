ad0c324a39287a666bd794071066afee05d0ec5f
herves <toundaherve@gmail.com>
2020-06-22 16:32:36 +0100

    Order creation: Browse menu

---
+++ db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"github.com/emma/emma"
+)
+
+// MenuService implements emma.MenuService
+type MenuService struct {
+}
+
+// MenuOfTheDay returns the menu from the store
+func (s *MenuService) MenuOfTheDay() (*emma.Menu, error) {
+	return &emma.Menu{
+		Foods: []emma.Food{
+			emma.Food{
+				Name: "name1",
+				Description: "desc1",
+				Price: 10.00,
+				Image: "img1",
+			},
+			emma.Food{
+				Name: "name2",
+				Description: "desc2",
+				Price: 10.00,
+				Image: "img2",
+			},
+		},
+	}, nil
+}
+
+// NewMenuService creates a menu service
+func NewMenuService() (*MenuService, error) {
+	return &MenuService{}, nil
+}
---
+++ db/db_test.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"testing"
+)
+
+func TestMenuOfTheDay(t *testing.T) {
+	s, err := NewMenuService()
+	if err != nil {
+		t.Fatalf("Could not create user service: %v", err)
+	}
+
+	m, err := s.MenuOfTheDay()
+	if err != nil {
+		t.Fatalf("Could not get the menu of the day: %v", err)
+	}
+
+	if n := len(m.Foods); n <= 0 {
+		t.Fatalf("expected at least 1 food, got: %d", n)
+	}
+}
